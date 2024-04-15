101851e60fd5dc1a914df0e7c33f791d48cc4e00
Annedrew <1196244890@qq.com>
2024-04-15 19:45:05 +0200

    Implement sidebar

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Card from "../ui/dashboard/card/page";
+import Chart from "../ui/dashboard/chart/page";
+import Rightbar from "../ui/dashboard/rightbar/page";
+import Transaction from "../ui/dashboard/transaction/page";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 }
--- app/lib/data.js
+++ app/lib/data.js
@@ -11,6 +11,7 @@ import {
     MdLogout,
   } from "react-icons/md";
 
+
 export const sidebarItems = [
   {
     title: "Pages",
@@ -71,5 +72,43 @@ export const sidebarItems = [
         icon: <MdHelpCenter />,
       },
     ],
+  },
+];
+
+export const chartData = [
+  {
+    name: "Sun",
+    visit: 4000,
+    click: 2400,
+  },
+  {
+    name: "Mon",
+    visit: 3000,
+    click: 1398,
+  },
+  {
+    name: "Tue",
+    visit: 2000,
+    click: 3800,
+  },
+  {
+    name: "Wed",
+    visit: 2780,
+    click: 3908,
+  },
+  {
+    name: "Thu",
+    visit: 1890,
+    click: 4800,
+  },
+  {
+    name: "Fri",
+    visit: 2390,
+    click: 3800,
+  },
+  {
+    name: "Sat",
+    visit: 3490,
+    click: 4300,
   },
 ];
