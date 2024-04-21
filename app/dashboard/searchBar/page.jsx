1cdba2483c703db04e4d1a190f46643c198c4777
Annedrew <1196244890@qq.com>
2024-04-21 22:42:26 +0200

    improve search efficient.

--- app/dashboard/searchBar/page.jsx
+++ app/dashboard/searchBar/page.jsx
@@ -2,23 +2,24 @@
 import { MdSearch } from "react-icons/md";
 import styles from "@/app/ui/dashboard/searchbar/searchbar.module.css";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchBar({placeholder}) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const {replace} = useRouter();
   
-  const handleSearch = (e) => {
+  const handleSearch = useDebouncedCallback((e) => {
     const params = new URLSearchParams(searchParams);
     
     if (e.target.value) {
-      params.set("q", e.target.value);
+      e.target.value.length > 1 && params.set("q", e.target.value);
     } else {
       params.delete("q");
     }
     params.set("test", "value");
     replace(`${pathname}?${params}`);
-  }
+  }, 300)
   
   return (
     <div className={styles.container}>
